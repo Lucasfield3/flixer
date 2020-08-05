a337064ab15025ca496f576d9fdd6f0fe2d4fca5
Lucasfield3 <lucasclic@yahho.com.br>
2020-08-05 20:31:06 -0300

    comite

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,49 +1,49 @@
 import React from 'react';
+// eslint-disable-next-line import/extensions
 import Menu from '../../componentes/Menu/index.js';
 import BannerMain from '../../componentes/BannerMain';
 import Carousel from '../../componentes/Carousel';
 import Footer from '../../componentes/Footer';
 import dadosIniciais from '../../data/dados_iniciais.json';
 
-
 function Home() {
   return (
-      <div style={{ background: "#141414" }}>
-        <Menu />
+    <div style={{ background: '#141414' }}>
+      <Menu />
 
-        <BannerMain 
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end"}
-        />
+        videoDescription="O que é Front-end"
+      />
 
-        <Carousel 
+      <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
-        />
+      />
 
-        <Carousel 
-          category={dadosIniciais.categorias[1]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
 
-        <Carousel 
-          category={dadosIniciais.categorias[2]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
 
-        <Carousel 
-          category={dadosIniciais.categorias[3]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
 
-        <Carousel 
-          category={dadosIniciais.categorias[4]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
 
-        <Carousel 
-          category={dadosIniciais.categorias[5]}
-        />
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />
 
-        <Footer />
-      </div>
+      <Footer />
+    </div>
   );
 }
 
